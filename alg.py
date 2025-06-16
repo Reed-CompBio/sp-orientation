f5932b440f60a44039f04cc865bd1d7af986b73d
Tristan F. <LeoDog896@hotmail.com>
2025-06-16 08:57:51 -0700

    feat: flow variables

--- alg.py
+++ alg.py
@@ -4,6 +4,7 @@ import itertools
 from ortools.linear_solver import pywraplp
 from pathlib import Path
 from mixed import Direction, MixedGraph
+from typing import cast
 
 
 def parse_args() -> argparse.Namespace:
@@ -113,17 +114,30 @@ def prepare_orientation_constraints(
         orientation_variable_reversed = orientations[(target, source)]
         orientation_constraint_reversed.SetCoefficient(orientation_variable_reversed, 1)
 
+def prepare_closure_variables(
+    solver: pywraplp.Solver, source_to_targets: dict[str, list[str]]
+) -> dict[tuple[str, str], pywraplp.Variable]:
+    # We define c_(s, t) as a {0, 1} variable that is satisfied when it is equal to 1.
+    # These check if the shortest path from (s, t) is properly satisfied by the directionality ILP.
+    closure_variables: dict[tuple[str, str], pywraplp.Variable] = {}
+    for source, targets in source_to_targets.items():
+        for target in targets:
+            closure_var: pywraplp.Variable = solver.IntVar(0, 1, f"c-{source}-{target}")
+            closure_variables[(source, target)] = closure_var
 
-def prepare_flow_variables(
+    return closure_variables
+
+def prepare_flow(
     solver: pywraplp.Solver,
     graph: MixedGraph[str],
-    sources: list[str],
+    source_to_targets: dict[str, list[str]],
     orientations: dict[tuple[str, str], pywraplp.Variable],
+    closures: dict[tuple[str, str], pywraplp.Variable]
 ):
-    for source in sources:
+    for source, targets in source_to_targets.items():
         edges: list[tuple[str, str]] = []
         paths: list[list[str]] = []
-        for target in graph.neighbors(source):
+        for target in targets:
             path = graph.dijkstra(source, target, "weight")
             paths.append(path)
 
@@ -132,27 +146,31 @@ def prepare_flow_variables(
                 t = path[i + 1]
                 edges.append((s, t))
 
+        edge_variables: dict[str, dict[str, pywraplp.Variable]] = dict()
+        edge_variables_reversed: dict[str, dict[str, pywraplp.Variable]] = dict()
         for s, t in edges:
-            # We first add edge flow s.t. it must be positive
-            edge_variable = solver.NumVar(0, solver.Infinity, f"edge-{s}-{t}")
+            # (10) We first add edge flow s.t. it must be positive along
+            # the shortest paths
+            edge_variable = solver.NumVar(0, solver.Infinity, f"{source}-edge-{s}-{t}")
             solver.Add(0 < edge_variable)
+            if not edge_variables[s] or not edge_variable[t]:
+                edge_variables[s] = dict()
+                edge_variables_reversed[t] = dict()
+            edge_variables[s][t] = edge_variable
+            edge_variables_reversed[t][s] = edge_variable
 
-            # then, restrict edge flow
+            # (11) then, we ensure that this flow respects all of the orientations we have
             solver.Add(edge_variable <= orientations[(s, t)])
 
+            # (12) we restrict the amount of flow exiting this edge to be less than the amount
+            # of flow entering it
+            # We use cast here since Variable doesn't know it can be added. This also holds for (13).
+            solver.Add(edge_variable <= sum(cast(int, i) for i in edge_variables_reversed[s].values()))
 
-def prepare_closure_variables(
-    solver: pywraplp.Solver, sources: list[str], targets: list[str]
-) -> dict[tuple[str, str], pywraplp.Variable]:
-    # We define c_(s, t) as a {0, 1} variable that is satisfied when it is equal to 1.
-    # These check if the shortest path from (s, t) is properly satisfied by the directionality ILP.
-    closure_variables: dict[tuple[str, str], pywraplp.Variable] = {}
-    for source, target in itertools.product(sources, targets):
-        closure_var: pywraplp.Variable = solver.IntVar(0, 1, f"c-{source}-{target}")
-        closure_variables[(source, target)] = closure_var
 
-    return closure_variables
-
+        # (13) restrict closure variables to require flow
+        for target in targets:
+            solver.Add(cast(int, closures[(source, target)]) <= sum(itertools.chain.from_iterable((cast(int, v) for t, v in t.items()) for s, t in edge_variables.items())))
 
 def main():
     args = parse_args()
@@ -160,11 +178,18 @@ def main():
     sources = read_nodes(args.sources)
     targets = read_nodes(args.targets)
 
+    source_to_targets = dict((a, [b]) for a, b in itertools.product(sources, targets))
+
     solver: pywraplp.Solver = pywraplp.Solver.CreateSolver("GLOP")
 
     orientations = prepare_orientation_variables(solver, interactome)
     prepare_orientation_constraints(solver, interactome, orientations)
+    closures = prepare_closure_variables(solver, source_to_targets)
+    prepare_flow(solver, interactome, source_to_targets, orientations, closures)
 
+    solver.Solve()
+
+    # TODO: output solver result
 
 if __name__ == "__main__":
     main()
